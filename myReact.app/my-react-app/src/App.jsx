19538730755de37afbe844fe3941aa0fc0175226
shivam <shivamprajapat9720@gmail.com>
2026-05-31 20:12:51 +0530

    first commit

---
+++ myReact.app/my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Student from './Student'
+
+function App() {
+  return (
+    <>
+      <h1>Student Information</h1>
+
+      <Student name="Shivam" course="B.Tech" marks="90" />
+      <Student name="Ansh" course="MCA" marks="95" />
+      <Student name="Akshat" course="BCA" marks="88" />
+    </>
+  )
+}
+
+export default App
